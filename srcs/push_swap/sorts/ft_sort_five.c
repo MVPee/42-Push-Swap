1ee93f41acbb88e84b07f2de20eac5fdb20f90c1
MVPee <mariusvanpee@hotmail.be>
2023-12-19 10:01:00 +0100

    add five and four, need to improve radix with rra or ra if condition

--- srcs/push_swap/sorts/ft_sort_five.c
+++ srcs/push_swap/sorts/ft_sort_five.c
@@ -6,13 +6,60 @@
 /*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:18:50 by mvpee             #+#    #+#             */
-/*   Updated: 2023/12/18 16:31:18 by mvpee            ###   ########.fr       */
+/*   Updated: 2023/12/19 09:48:21 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/push_swap.h"
 
+static void	second_push_b(t_stack **a, t_stack **b)
+{
+	t_stack			*temp;
+	unsigned int	count;
+
+	temp = *a;
+	count = 0;
+	while (temp->rank != 1)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (count > 3)
+	{
+		count = 5 - count;
+		while (count--)
+			rra(a);
+	}
+	else
+		while (count--)
+			ra(a);
+	pb(a, b);
+}
+
 void	ft_sort_five(t_stack **a, t_stack **b)
 {
-	return ;
+	t_stack			*temp;
+	unsigned int	count;
+
+	temp = *a;
+	count = 0;
+	while (temp->rank != 0)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (count > 3)
+	{
+		count = 5 - count;
+		while (count--)
+			rra(a);
+	}
+	else
+		while (count--)
+			ra(a);
+	pb(a, b);
+	second_push_b(a, b);
+	ft_sort_three(a);
+	pa(a, b);
+	pa(a, b);
 }
--- srcs/push_swap/sorts/ft_sort_four.c
+++ srcs/push_swap/sorts/ft_sort_four.c
@@ -6,7 +6,7 @@
 /*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:19:22 by mvpee             #+#    #+#             */
-/*   Updated: 2023/12/18 16:32:10 by mvpee            ###   ########.fr       */
+/*   Updated: 2023/12/19 09:16:52 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 void	ft_sort_four(t_stack **a, t_stack **b)
 {
-	return ;
+	t_stack *temp;
+	unsigned int count;
+	
+	temp = *a;
+	count = 0;
+	while(temp->rank != 0)
+	{
+		count++;
+		temp = temp->next;
+	}
+	while(count--)
+		ra(a);
+	pb(a, b);
+	ft_sort_three(a);
+	pa(a, b);
 }
